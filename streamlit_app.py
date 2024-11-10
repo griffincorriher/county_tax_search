9f392f83379567617bdab2da096265fcb4da89c9
Griffin Corriher <57311125+griffincorriher@users.noreply.github.com>
2024-11-10 02:08:33 +0000

    Started to add paste address feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,46 +2,50 @@ import streamlit as st
 import censusgeocode as cg
 
 st.subheader("Enter an address")
-col1, col2, col3, col4 = st.columns([2, 1, 1,1]) 
+tab1, tab2 = st.tabs(["Enter Address", "Paste Address"])
 
-with col1:
-    address = st.text_input("Address", placeholder="164 S Main St")
-with col2:
-    city = st.text_input("City", placeholder="Kannapolis")    
-with col3:
-    state = st.text_input("State", placeholder="NC")
-with col4:
-    zip = st.text_input("Zip Code", placeholder="28081")
-full_address = f"{address}  \n {city} {state}, {zip}"
-full_address_2 = f"{address}, {city}, {state}, {zip}"
-if isinstance(full_address,str):
-    try:
-        results = cg.onelineaddress(full_address_2)
-    except:
-        st.write('There is an problem with the address')
-    if len(results) > 0:
-        st.header("Found a matched address!")
-        st.write(f"{results[0]['matchedAddress']}")    
-        latitude = results[0]['coordinates']['y']  
-        longitude = results[0]['coordinates']['x']
-        latitude_str = str(results[0]['coordinates']['y'])
-        longitude_str = str(results[0]['coordinates']['x'])
-        data = {
-        'latitude': [latitude],
-        'longitude': [longitude]
-        }
-
-        result = results[0]['geographies']['Counties'][0]['NAME']
-        st.subheader(f"{result}")
-        st.map(data,
-                latitude=latitude_str,
-                longitude=longitude_str, 
-                use_container_width=True,
-                zoom=14)
-    else:
-        st.write("There was a problem with the address.")
+with tab1:
+    col1, col2, col3, col4 = st.columns([2, 1, 1,1]) 
+    with col1:
+        address = st.text_input("Address", placeholder="164 S Main St", key='address')
+    with col2:
+        city = st.text_input("City", placeholder="Kannapolis", key='city')
+    with col3:
+        state = st.text_input("State", placeholder="NC", key='state')
+    with col4:
+        zip = st.text_input("Zip Code", placeholder="28081", key='zip')
     if address and city and state and zip:
-        # st.success("All fields are filled out!")
-        pass
+        full_address = f"{address}, {city}, {state}, {zip}"
     else:
         st.error("Please fill in all the fields.")
+    if isinstance(address,str):
+        try:
+            results = cg.onelineaddress(full_address)
+            if len(results) > 0:
+                st.header("Found a matching address!")
+                st.write(f"{results[0]['matchedAddress']}")    
+                latitude = results[0]['coordinates']['y']  
+                longitude = results[0]['coordinates']['x']
+                latitude_str = str(results[0]['coordinates']['y'])
+                longitude_str = str(results[0]['coordinates']['x'])
+                data = {
+                'latitude': [latitude],
+                'longitude': [longitude]
+                }
+
+                result = results[0]['geographies']['Counties'][0]['NAME']
+                st.subheader(f"{result}")
+                st.map(data,
+                        latitude=latitude_str,
+                        longitude=longitude_str, 
+                        use_container_width=True,
+                        zoom=14)
+            else:
+                st.write("There was a problem with the address.")
+        except NameError as e:
+            pass
+with tab2:
+    st.write("Feature in progress")
+    # full_address = st.text_input(label='Full address', placeholder='164 S Main St Kannapolis, NC 28081')
+
+
